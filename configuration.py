4145bb656486818699da019b4648d177664e3c14
mihailverzhbitsky <verzbmihail@gmail.com>
2023-08-20 01:22:30 +0300

    OSUSHESTVLENA RABOTA PO ZADACHE

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Вержбицкий Михаил, 7-я когорта - Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://93e1caa6-e0b1-483f-9440-ddbf8da048af.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
